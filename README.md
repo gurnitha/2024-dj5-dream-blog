5b73dfb9ebf83444f20f2cb4d64c92b65bba3b07
kamubisaapakawan <kamubisaapakawan@gmail.com>
2024-02-23 11:19:21 +0700

    6. READ: Retrieve and fetch categories and number of posts related to each category

--- README.md
+++ README.md
@@ -874,3 +874,11 @@ Membangung Aplikasi Blog from Zero to Deployment menggunakan Django versi 5
         modified:   README.md
         modified:   apps/post/templates/post/blog.html
         modified:   apps/post/views.py
+
+
+#### 6. READ: Retrieve and fetch categories and number of posts related to each category
+
+        modified:   README.md
+        modified:   apps/post/templates/post/blog.html
+        modified:   apps/post/views.py
+
--- apps/post/views.py
+++ apps/post/views.py
@@ -2,6 +2,7 @@
 
 # Django and third parties modules
 from django.shortcuts import render
+from django.db.models import Count
 
 # Locals
 from apps.post.models import Post
@@ -19,14 +20,28 @@ def home_view(request):
 	}
 	return render(request, 'post/index.html', data)
 
+
+# Category count view
+def get_category_count():
+	# 1. Get all the post objects
+	# queryset = Post.objects.all()
+	# 2. Get the category names from the post objects
+	# queryset = Post.objects.values('categories') # <-- it will return only categories fields of each post
+	# 3. Count the number of times of each category occured
+	queryset = Post.objects.values('categories__title').annotate(Count('categories__title')) # <-- it will return only categories fields of each post
+	return queryset
+
 def blog_view(request):
 	post_list = Post.objects.all()
 	# print(post_list)
 	latest_posts = Post.objects.order_by('-timestamp')[0:3]
 	# print(latest_posts)
+	category_count = get_category_count()
+	print(category_count)
 	data = {
 		'post_list':post_list,
 		'latest_posts':latest_posts,
+		'category_count':category_count,
 	}
 	return render(request, 'post/blog.html', data)
 
